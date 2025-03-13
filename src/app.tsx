ad638a42f135000dee2b033b2cf273d528293dea
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-03-13 12:00:17 -0300

    feat: add a basic login form

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,31 @@
+import { type FormEvent, useState } from 'react';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+
 export function App() {
-	return <h1>Hello, World!</h1>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		console.log({
+			email,
+			password
+		});
+	}
+
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-zinc-950 text-zinc-50">
+			<form onSubmit={handleFormSubmit} className="space-y-4">
+				<div className="space-y-2">
+					<Input type="email" placeholder="E-mail" value={email} onChange={(event) => setEmail(event.target.value)} />
+					<Input type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+				</div>
+				<Button type="submit" variant="secondary" className="w-full cursor-pointer">
+					Submit
+				</Button>
+			</form>
+		</div>
+	);
 }
